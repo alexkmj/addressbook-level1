5cd4d4d67b8ae80b6a9261401f36922b69f60736
alex <alex_koh_@live.com>
2018-08-31 15:26:12 +0800

    Add support for whatsapp

--- src/test/java/seedu/addressbook/AddressBookTest.java
+++ src/test/java/seedu/addressbook/AddressBookTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 import java.io.*;
 
 public class AddressBookTest {
-    @Test public void testFileEqual() throws FileNotFoundException {
-       //TODO Create a test to compare actual and result
+    @Test
+    public void testFileEqual() throws FileNotFoundException {
+        //TODO Create a test to compare actual and result
     }
 }
